ad9db17ed09cae7802130d2e509f772399a9f49b
leesh <leesh7807@gmail.com>
2023-03-27 16:57:58 +0900

    solve

--- Algorithm/1080.cpp
+++ Algorithm/1080.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main() {
     int N, M;
     cin >> N >> M;
-    char A[N][M], B[N][M];
+    char A[N+1][M+1], B[N+1][M+1]; // \0을 고려해서 +1. 컴파일러에 따라서 안해줘도 잘 작동하지만 위험하다. 
+    // 입력 받기
     for(int i = 0; i < N; i++) {
         cin >> A[i];
     }
     for(int i = 0; i < N; i++) {
         cin >> B[i];
     }
+    // 행 또는 열이 3보다 작으면 두 행렬이 같은지 확인 다르면 -1 아니면 0 출력 후 리턴
     if(N < 3 || M < 3) { 
         for(int i = 0; i < N; i++) {
             for(int j= 0; j < M; j++) {
@@ -20,6 +22,9 @@ int main() {
         cout << 0; return 0;
     } 
 
+    // 3X3 부분 행렬의 (0+i, 0+j)를 기준으로 (2+i, 2+j)까지 뒤집는 것으로 가정.
+    // nXm 행렬을 훑을 때 (i, j)를 뒤집지 않으면 임의의 (i+k, j+k)에서 뒤집는 연산을 하여도 (i, j)는 뒤집히지 않음.
+    // 따라서, (i, j)는 무조건 뒤집기를 해야하는 최소 횟수라 가정. 이러한 뒤집기 연산은 (N-2, M-2)까지만 가능하다.
     int count = 0;
     for(int i = 0; i < N-2; i++) {
         for(int j = 0; j < M-2; j++) {
@@ -35,6 +40,7 @@ int main() {
         }
     }
 
+    // A와 B 전체 행렬 비교
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
             if(A[i][j] != B[i][j]) { cout << -1; return 0; }
--- Algorithm/1105.cpp
+++ Algorithm/1105.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     int L, R;
     cin >> L >> R;
-    int i = L;
-    
-    while(i <= R) {
-        if(i % 10 != 8) { i /= 10; R /= 10; } // 1의 자리가 8이 아니면 1의 자리를 날린다.
-        i++;
+    string strL = to_string(L);
+    string strR = to_string(R);
+    if(strL.length() != strR.length()) { cout << 0; return 0; } // 자리 수가 다르면 무조건 0이 나온다.
+
+    int eight = 0;
+    for(int i = 0; i < strL.length(); i++) {
+        if((strL[i] == '8') && (strR[i] == '8')) { eight++; } // 가장 큰 자리부터 비교하여 8이 나오면 count++
+        else if(strL[i] == strR[i]) { continue; } // 특정 자리수가 다르면 그 아래 자리수들은 전부 1이상 더할 수 있다. 절대 8이 아니다.
+        else { break; }
     }
-    int result = i - 1;
-    int eightCount = 0;
-    while(result != 0) {
-        if(result % 10 == 8) { eightCount++; }
-        result /= 10;
-    }
-    cout << eightCount;
+    cout << eight;
     return 0;
 }
