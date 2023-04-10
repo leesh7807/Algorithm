f941e11bc972fbff421777bfb8e93bc20655bb0f
leesh <leesh7807@gmail.com>
2023-04-10 19:43:18 +0900

    1016, 1517 solve

--- Algorithm/1016.cpp
+++ Algorithm/1016.cpp
@@ -3,37 +3,52 @@
 #include <vector>
 #define endl '\n'
 using namespace std;
-bool arr[1001000] = {false,};
-// log1조는 40으로 일반적으로 40번 안에 소인수분해가 끝날 것.
-bool Factorization(long long int n) {
-    int i= 2;
-    do {
-        int cnt = 0;
-        while(n%i == 0) {
-            for(int j=1; i*j<1001000; j++) {
-                arr[i*j] = true; 
-            }
-            n /= i;
-            cnt++;
-        }
-        if(cnt > 1) return false; // 제곱ㄴㄴ수가 아니다.
-        for(int j=2; j<1001000; j++) {
-            if(!arr[j]) i = j;
-        }
-    } while(n>1);
-    return true; // 제곱ㄴㄴ수가 맞다.
-};
+
+long long int arr[1000001];
+bool prime[1000001];
+
 // 2초 512MB
-// 소인수분해 했을 때, 제곱인 것이 없으면 제곱ㄴㄴ수
 // 범위 내 제곱ㄴㄴ수
-// 소인수분해는 logn으로 줄어들 것. log1조 = 40. 40*백만= 사천만
 int main() {
-    long long int min, max; // 1조, 1조+백만
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    long long int min, max; // 1조, min과 백만차이.
     cin >> min >> max;
-    int ans = 0;
-    for(long long int i = min; i<=max; i++) { // 백만번
-        if(Factorization(i)) ans++;
+    for(int i=2; i<1000001; i++) {
+        prime[i] = true;
     }
+    for(int i=0; i <= max-min; i++) {
+        arr[i] = i+min;
+    }
+    for(int i=2; i<1000001; i++) { // 소수 찾기
+        if(prime[i]) {
+            for(int j=i*2; j<1000001; j+=i) {
+                prime[j] = false;
+            }
+        }
+    }
+    for(long long int i=2; i*i <= max; i++) {
+        if(prime[i]) {
+            if(min%(i*i) == 0) {
+                for(long long int j=0; j <= max-min; j += i*i) {
+                    arr[j] = 0;
+                }
+            }
+            else {
+                // 특정 제곱으로 나눠지는지 확인하기 위해 범위를 다 확인할 필요가 없다.
+                // ex) 30/4 = 7.xxx.     7*4 + 4 = 32가 범위 내의 최초 값. 이 값부터 배수들을 전부 제거하면 된다.
+                // 1부터 1000000까지 70000개 이상의 소수 중 200개만 전수 조사하더라도 2억번의 연산이 들어간다.
+                for(long long int j = (min/(i*i) + 1) * (i*i) - min; j <= max-min; j += i*i) {
+                    arr[j] = 0;
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for(int i=0; i <= max-min; i++) {
+        if(arr[i]) ans++;
+    }
+
     cout << ans;
     return 0;
 }
