9cc693c7208be09a557193524d0e160a8bdd36cb
leesh <leesh7807@gmail.com>
2023-05-25 21:47:35 +0900

    15649 풀었음

--- JAVA/src/j15649.java
+++ JAVA/src/j15649.java
@@ -3,27 +3,36 @@ import java.io.*;
 public class j15649 {
     static int n;
     static int m;
+    static boolean[] visited;
+    static ArrayList<Integer> stack;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        LinkedHashSet<Integer> hs = new LinkedHashSet<Integer>();
-
-        
+        for(int i=1; i<=n; i++) {
+            stack = new ArrayList<>();
+            visited = new boolean[n+1];
+            backtracking(i);
+        }
     }
 
-    public static void backtracking(int root, int size, boolean[] isVisited) throws Exception{
-        
+    public static void backtracking(int v) throws Exception{
+        stack.add(v);
+        visited[v] = true;
+        if(stack.size() == m) {
+            StringBuilder sb = new StringBuilder();
+            for(Integer i : stack) {
+                sb.append(Long.toString(i) + ' ');
+            }
+            System.out.println(sb);
+        }
+        for(int i=1; i<=n; i++) {
+            if(!visited[i]) {
+                backtracking(i);
+                stack.remove(stack.size() - 1);
+                visited[i] = false;
+            }
+        }
     }
 }
-
-
-/* 1 2 3
- * 1 2 4
- * 1 3 2
- * 1 3 4
- * 1 4 2
- * 1 4 3
- * 2 1 3
- */
