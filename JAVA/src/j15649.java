aa5657647e4621bb5c1fffe79d2cd85a57338cbb
leesh <leesh7807@gmail.com>
2023-05-25 14:55:53 +0900

    15649 푸는중

---
+++ JAVA/src/j15649.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class j15649 {
+    static int n;
+    static int m;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        LinkedHashSet<Integer> hs = new LinkedHashSet<Integer>();
+
+        
+    }
+
+    public static void backtracking(int root, int size, boolean[] isVisited) throws Exception{
+        
+    }
+}
+
+
+/* 1 2 3
+ * 1 2 4
+ * 1 3 2
+ * 1 3 4
+ * 1 4 2
+ * 1 4 3
+ * 2 1 3
+ */
