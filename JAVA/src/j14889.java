e64e0aff7fedfc702ee27eb257b39e366ac54d4b
leesh <leesh7807@gmail.com>
2023-05-30 21:09:42 +0900

    14889 푸는 중

--- JAVA/src/j14889.java
+++ JAVA/src/j14889.java
@@ -1,3 +1,27 @@
+import java.util.*;
+import java.io.*;
 public class j14889 {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+    }
+    public static void comb(int depth) {
+        
+    }
 }
+
+/* 20C10 * 20^2 브루트포스 시간복잡도 
+ * 조합 연산할 때 20Cn 정도가 대충 18만이라고 어림짐작
+ * 거기서부터 1커질때마다 대충 2배씩 뛴다고 생각하면 될듯.
+ * 20만 * 400는 8천만 정도.
+*/
