56ce7efa4368c7c55c323e479ef13bcb945a2e5d
leesh <leesh7807@gmail.com>
2023-06-03 22:40:47 +0900

    7576 품

---
+++ JAVA/src/j7576.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+public class j7576 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int col = Integer.parseInt(st.nextToken());
+        int row = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[row][col];
+        LinkedList<Pair> tomato = new LinkedList<>();
+        for(int i=0; i<row; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<col; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 1) {
+                    tomato.add(new Pair(i, j));
+                }
+            }
+        }
+        int day = 0;
+        
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while(!tomato.isEmpty()) {
+            day++;
+            LinkedList<Pair> nextDay = new LinkedList<>();
+            while(!tomato.isEmpty()) {
+                nextDay.add(tomato.poll());                
+            }
+            for(Pair p : nextDay) {
+                for(int i=0; i<4; i++) {
+                    int newX = p.x+dx[i];
+                    int newY = p.y+dy[i];
+                    if(newX >= 0 && newX < row && newY >= 0 && newY < col && arr[newX][newY] == 0) { 
+                        arr[newX][newY] = 1;
+                        tomato.add(new Pair(newX, newY));
+                    }
+                }
+            }
+        }
+        if(isRiped(row, col, arr)) {
+            bw.write(Long.toString(day-1));
+        }
+        else {
+            bw.write("-1");
+        }
+        bw.close();
+    }
+
+    public static boolean isRiped(int row, int col, int[][] arr) { // 다 익었으면 true 아님 flase
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(arr[i][j] == -1) {
+                    continue;
+                }
+                else if(arr[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    static class Pair {
+        int x, y;
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
