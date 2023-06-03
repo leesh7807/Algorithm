47996a064f6d8e777bf7fe2685d241de04509745
leesh <leesh7807@gmail.com>
2023-06-03 13:22:43 +0900

    15591 푸는 중

--- JAVA/src/j15591.java
+++ JAVA/src/j15591.java
@@ -8,40 +8,33 @@ public class j15591 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n+1][n+1];
+        ArrayList<?>[] arr = new ArrayList<?>[n+1];
+        for(int i=1; i<=n; i++) {
+            arr[i] = new ArrayList<Pair>();
+        }
         for(int i=0; i<n-1; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            arr[r][c] = v;
-            arr[c][r] = v;
+            arr[r].add(new Pair(c, v));
         }
         for(int i=0; i<q; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            bw.write(Long.toString(bfs(v, k, arr)) + '\n');
+            
         }
-        bw.close();
+        bw.close();   
     }
-    public static int bfs(int v, int k, int[][] arr) {
-        int cnt = 0;
-        LinkedList<Integer> queue = new LinkedList<>();
-        boolean[] isVisited = new boolean[n+1];
-        queue.add(v);
-        isVisited[v] = true;
-        while(!queue.isEmpty()) {
-            int now = queue.peekFirst();
-            for(int i=1; i<=n; i++) {
-                if(!isVisited[i] && arr[now][i] >= k) {
-                    isVisited[i] = true;
-                    queue.add(i);
-                    cnt++;
-                }
-            }
-            queue.poll();
+    public static void bfs() {
+
+    }
+    static class Pair {
+        int x, y;
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        return cnt;
     }
 }
