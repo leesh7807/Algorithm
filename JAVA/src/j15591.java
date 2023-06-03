3f6e76e6d3f72321cc78950a5a983bb0f1f78103
leesh <leesh7807@gmail.com>
2023-06-03 21:53:52 +0900

    15591 풀었음

--- JAVA/src/j15591.java
+++ JAVA/src/j15591.java
@@ -2,39 +2,66 @@ import java.util.*;
 import java.io.*;
 public class j15591 {
     static int n;
+    static ArrayList<ArrayList<Pair>> arr;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
-        ArrayList<?>[] arr = new ArrayList<?>[n+1];
-        for(int i=1; i<=n; i++) {
-            arr[i] = new ArrayList<Pair>();
+        arr = new ArrayList<ArrayList<Pair>>();
+        arr.add(new ArrayList<Pair>(Arrays.asList(new Pair(0, 0)))); // 0번째 비디오에 더미 값.
+        for(int i=0; i<n; i++) { // arr 초기화.
+            arr.add(new ArrayList<Pair>());
         }
         for(int i=0; i<n-1; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr[r].add(new Pair(c, v));
+            int w = Integer.parseInt(st.nextToken());
+            arr.get(r).add(new Pair(c, w));
+            arr.get(c).add(new Pair(r, w));
         }
         for(int i=0; i<q; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            
+            int video = bfsSearch(k, v);
+            bw.write(Long.toString(video) + "\n");
         }
         bw.close();   
     }
-    public static void bfs() {
-
+    public static int bfsSearch(int k, int v) {
+        LinkedList<Integer> q = new LinkedList<>();
+        boolean[] isVisited = new boolean[n+1];
+        q.add(v);
+        isVisited[v] = true;
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            for(Pair p : arr.get(now)) {
+                if(!isVisited[p.d] && p.w >= k) {
+                    isVisited[p.d] = true;
+                    q.add(p.d);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
     static class Pair {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int d, w;
+        Pair(int d, int w) {
+            this.d = d;
+            this.w = w;
         }
     }
 }
+
+/* 인접행렬에서 dfs or bfs시 시간복잡도는 V^2
+ * 인접리스트에서 시간복잡도는 V+E.
+ * 무방향 그래프에서 간선의 최대 개수는 n*(n-1)/2. 
+ * 이 문제의 경우 간선의 최대 개수는 n-1로 고정되어있음.
+ * 인접 행렬로 풀 경우 5000(정점) * 5000(간선) * 5000(질문).
+ * 인접 리스트로 풀 경우 ( 5000(정점) + 5000(간선) ) * 5000(질문). 
+*/
