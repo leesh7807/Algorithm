51a29f08abfddb5b2b52bab7e041d3761add4fb5
leesh <leesh7807@gmail.com>
2023-06-11 16:13:12 +0900

    1003 품

--- JAVA/src/j1003.java
+++ JAVA/src/j1003.java
@@ -1,14 +1,22 @@
-import java.util.*;
 import java.io.*;
 public class j1003 {
-    static int[][] cache = new int[40][40];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[][] fibo = new int[41][2];
+        fibo[0][0] = 1;
+        fibo[0][1] = 0;
+        fibo[1][0] = 0;
+        fibo[1][1] = 1;
+        for(int i=2; i<41; i++) {
+            fibo[i][0] = fibo[i-1][0] + fibo[i-2][0];
+            fibo[i][1] = fibo[i-1][1] + fibo[i-2][1];
+        }
         int T = Integer.parseInt(br.readLine());
         for(int i=0; i<T; i++) {
             int N = Integer.parseInt(br.readLine());
-
+            bw.write(Long.toString(fibo[N][0]) + ' ' + Long.toString(fibo[N][1]) + '\n');
         }
+        bw.close();
     }
 }
