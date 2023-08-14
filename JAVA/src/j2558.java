d1a165911cb9d3151b87543d07cf40877970ab97
leesh <leesh7807@gmail.com>
2023-08-15 02:56:03 +0900

    just

---
+++ JAVA/src/j2558.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class j2558 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A+B);
+    }
+}
